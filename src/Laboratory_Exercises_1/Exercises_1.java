7e3b13e4686b657f828176b05ff0f8488d567964
ridvancakirtr <ridvancakirtr@gmail.com>
2018-04-09 11:05:04 +0300

    Add Comma

--- src/Laboratory_Exercises_1/Exercises_1.java
+++ src/Laboratory_Exercises_1/Exercises_1.java
@@ -1,41 +1,27 @@
 package Laboratory_Exercises_1;
 
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Exercises_1 {
-    private String Name;
+    private int TotalStudent=2;
+    private String Name[]=new String[TotalStudent];
     private int Midterm,Final;
-    private double Avarage;
+    private double[] Avarage=new double[TotalStudent];
     private double TotalAvarage=0.0;
     Exercises_1(){
-        for (int i=0;i<2;i++){
+        for (int i=0;i<TotalStudent;i++){
             //Separate with commas by entering values
             String studentNameMidtermFinal= JOptionPane.showInputDialog(null,"Student Name,Midterm,Final ?");
-            separeteComma(studentNameMidtermFinal);
-            System.out.println("Name : "+Name+"   Avarage : "+Avarage);
+            String[] data=studentNameMidtermFinal.split(",");
+            Name[i]=data[0];
+            Midterm=Integer.parseInt(data[1]);
+            Final=Integer.parseInt(data[2]);
+            Avarage[i]=(Midterm*0.40+Final*0.60);
+            TotalAvarage+= Avarage[i]/TotalStudent;
         }
-        TotalAvarage=TotalAvarage/2;
-        JOptionPane.showMessageDialog(null,"Total Avarage : "+TotalAvarage);
-    }
 
-
-    private void separeteComma(String studentNameMidtermFinal) {
-        String[] data=studentNameMidtermFinal.split(",");
-        Name=data[0];
-        Midterm=Integer.parseInt(data[1]);
-        Final=Integer.parseInt(data[2]);
-        Avarage=Midterm*0.40+Final*0.60;
-        TotalAvarage+=Avarage;
-    }
-}
-class student{
-    private String sname;
-    private Double savg;
-
-    public student(String name, Double avarage) {
-        this.sname = name;
-        this.savg = avarage;
+        for (int i=0;i<TotalStudent;i++){
+            System.out.println("Name : "+Name[i]+"\tAvg : "+Avarage[i]+"\tTotal Avg : "+TotalAvarage);
+        }
     }
 }
